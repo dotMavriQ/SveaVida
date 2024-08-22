67f7e995cf083eb0ba874cde0f98e522386f10a8
dotMavriQ <dotmavriq@dotmavriq.life>
2024-08-22 23:17:08 +0100

    fixed snus emoji at least, reverted to better map

--- app.js
+++ app.js
@@ -1,75 +1,56 @@
 var map = L.map('map').setView([39.3999, -8.2245], 7); // Centers the map on Portugal
 
-// Grayscale layer for the whole world
-var grayscale = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-// GeoJSON data for Portugal border
-var portugalBorder = {
-    "type": "Feature",
-    "properties": {},
-    "geometry": {
-        "type": "Polygon",
-        "coordinates": [[
-            [-9.294024294082845, 36.7472710948788],
-            [-7.202243352977746, 36.8122056944769],
-            [-7.45230633834521, 37.55964862005044],
-            [-6.899397518349758, 38.07573050232611],
-            [-6.925345147129605, 39.06505025549865],
-            [-6.126969031558218, 41.608586064367984],
-            [-6.5122506328414715, 42.054293420642836],
-            [-9.182761709795727, 42.25696617476794],
-            [-10.490522740966469, 38.47554775384995],
-            [-9.30850585385889, 36.78329083735892],
-            [-9.294024294082845, 36.7472710948788] // Closing the polygon
-        ]]
-    }
-};
-
-// Add GeoJSON layer for the Portugal border
-var portugalArea = L.geoJSON(portugalBorder, {
-    style: {
-        color: "blue", // Border color
-        fillColor: "rgba(255, 255, 255, 0.5)", // Fill color
-        fillOpacity: 0.5,
-        weight: 1
-    }
-}).addTo(map);
-
-// Function to add markers
-var markers = {};
+var markers = {}; // Object to hold markers
 
 function addMarker(place) {
-    var marker = L.marker([place.Lat, place.Long]).addTo(map)
-        .bindPopup(`<strong>${place.Placename}</strong><br>Products: ${place.Products.join(", ")}<br><a href="${place.Link}" target="_blank">More info</a>`);
-    markers[place.Placename] = marker;
+    var iconUrl = '';
+    if (place.Products.includes('Grädde')) {
+        iconUrl = 'gradde.png';
+    } else if (place.Products.includes('Snus')) {
+        iconUrl = 'snus.png';
+    } else if (place.Products.includes('Svenska varor')) {
+        iconUrl = 'svenskavaror.png';
+    }
+
+    if (iconUrl) { // Ensure iconUrl is not empty
+        var customIcon = L.icon({
+            iconUrl: iconUrl,
+            iconSize: [35, 35],
+            iconAnchor: [17, 35],
+            popupAnchor: [0, -35]
+        });
+
+        var marker = L.marker([place.Lat, place.Long], {icon: customIcon}).bindPopup(`<strong>${place.Placename}</strong><br>Products: ${place.Products.join(", ")}<br><a href="${place.Link}" target="_blank">More info</a>`);
+        markers[place.Placename] = marker; // Store marker
+    }
 }
 
-// Load places data
+// Initially, do not add markers to map
 fetch('places.json')
     .then(response => response.json())
     .then(data => {
         data.forEach(place => {
-            addMarker(place);
+            addMarker(place); // Just create markers, do not add to map yet
         });
     });
 
-// Handle checkbox changes
 document.getElementById('productForm').addEventListener('change', function(e) {
-    if (e.target.type === 'checkbox') {
-        fetch('places.json')
-            .then(response => response.json())
-            .then(data => {
-                data.forEach(place => {
-                    if (place.Products.includes(e.target.name)) {
-                        if (e.target.checked) {
-                            addMarker(place);
-                        } else {
-                            removeMarker(place);
-                        }
+    var product = e.target.name.toLowerCase();
+    fetch('places.json')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(place => {
+                if (place.Products.map(p => p.toLowerCase()).includes(product)) {
+                    if (e.target.checked && !map.hasLayer(markers[place.Placename])) {
+                        markers[place.Placename].addTo(map);
+                    } else if (!e.target.checked && map.hasLayer(markers[place.Placename])) {
+                        map.removeLayer(markers[place.Placename]);
                     }
-                });
+                }
             });
-    }
+        });
 });
